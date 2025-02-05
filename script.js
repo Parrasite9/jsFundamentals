20e77cc973c21b1774ea68f3e62eaa621764e7bd
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2025-02-04 20:32:44 -0600

    Javascript fundamentals completed

---
+++ script.js
@@ -0,0 +1,29 @@
+// console.log("Hello World");
+
+// var 
+// I WILL NEVER USE VAR 
+// I WILL NEVER USE VAR 
+// I WILL NEVER EVER EVER NEVER EVER EVER USE VAR 
+
+let name = "Isaiah Johnson"; // Quotation marks is a string
+const age = 25; // whole numbers are integers
+
+// console.log(name);
+// console.log(age);
+
+// console.log("Hello" + " " + name);
+// console.log("Hello " + name);
+// console.log("Hello " + name + ". Your age is " + age);
+// console.log(`Hello ${name}, you are ${age} years old`);
+
+// let firstName = "Isaiah";
+// let lastName = "Johnson";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+// npm i readline-sync, Installs prompt inputs for node (Terminal)
+const readline = require('readline-sync')
+
+// let userName = readline.question("What is your Name? ");
+// console.log(`Hello ${userName}`);
